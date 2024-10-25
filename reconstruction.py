751707bba2dfde13f022505cee56c599afcb2ee3
Nirav <niravbhattad24@gmail.com>
2024-10-25 09:38:08 +0530

    adding csv files first

--- reconstruction.py
+++ reconstruction.py
@@ -9,11 +9,11 @@ def mfcc_to_audio(mfcc, sr=44100, hop_length=512, n_mels=128, n_fft=2048, iterat
     audio = librosa.feature.inverse.mel_to_audio(mel_spectrogram, sr=sr, hop_length=hop_length, n_fft=n_fft, n_iter=iterations)
     return audio
 
-for i in range(1, 116):
+for i in range(100, 117):
     curr_time = time.time()
-    mfcc = np.loadtxt('data/' + ('0' if i < 10 else '') + str(i) + '-MFCC.csv', delimiter=',')
+    mfcc = np.loadtxt('data-v2/' + ('0' if i < 10 else '') + str(i) + '-MFCC.csv', delimiter=',')
     reconstructed_audio = mfcc_to_audio(mfcc)
-    sf.write('reconstructed-data/' + str(i) + '.wav', reconstructed_audio, 44100)
+    sf.write('reconstructed-data-v2/' + str(i) + '.wav', reconstructed_audio, 44100)
 
     print('Reconstructed audio saved as reconstructed-data-' + str(i) + '.wav at time ' + str(time.time() - curr_time) + ' seconds')
 
